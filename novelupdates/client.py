94ef93e84091ef7484ea8da16ac7181defc6f626
lina <jack@jackli.dev>
2022-10-13 20:33:47 -0700

    feat: search novels

--- novelupdates/client.py
+++ novelupdates/client.py
@@ -1,14 +1,29 @@
 from .request import Request
-from .parseFeed import parseFeed
+from . import parsers
 
 
 class Client:
     def __init__(self):
-        """
-        Initialize the client.
-        """
         self.req = Request()
         
-    def getLatestFeed(self):
+    def get_latest_feed(self):
+        """Gets the latest updates from NovelUpdates.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        :class:`dict`
+            A dictionary containing the latest novel updates from NovelUpdates.
+            Contains all information and links for each update.
+        """
         req = self.req.get("https://www.novelupdates.com/")
-        return parseFeed(req)
+        return parsers.parseFeed(req)
+
+    def search_series(self, name):
+        """Searches for a series and gets back the top 10 results.
+        """
+        req = self.req.get(f"https://www.novelupdates.com/?s={name}")
+        return parsers.parseSearch(req)
---
+++ novelupdates/parsers.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup as bs
+import re
+
+def parseFeed(req):
+    soup = bs(req.text, "html.parser")
+    feed = []
+    for table in soup.find_all("table", id="myTable", class_="tablesorter"):
+        for entry in table.find("tbody").find_all("tr"):
+            title = entry.select("td")[0].find("a").get("title")
+            nuLink = entry.select("td")[0].find("a").get("href")
+            chapter = entry.select("td")[1].find("a").get("title")
+            chapterLink = f'https:{entry.select("td")[1].find("a").get("href")}'
+            groupName = entry.select("td")[2].find("a").get("title")
+            groupLink = entry.select("td")[2].find("a").get("href")
+            feed.append({
+                "title": title,
+                "nuLink": nuLink,
+                "chapter": chapter,
+                "chapterLink": chapterLink,
+                "groupName": groupName,
+                "groupLink": groupLink
+            })
+    return feed
+
+def parseSearch(req):
+    soup = bs(req.text, "html.parser")
+    results = []
+    for result in soup.find_all("div", class_="search_main_box_nu"):
+        body = result.find("div", class_="search_body_nu")
+        imageBody = result.find("div", class_="search_img_nu")
+
+        title = body.find("div", class_="search_title").find("a").text
+        link = body.find("div", class_="search_title").find("a").get("href")
+        
+        image = imageBody.find("img").get("src")
+        if image.endswith("noimagemid.jpg"):
+            image = None
+        imageBody.find("div", class_="search_ratings").find("span", class_="orgjp").decompose()
+        searchRating = re.sub(r'[()]', '', imageBody.find("div", class_="search_ratings").text.strip())
+
+        ogDescription = body.find(text=True, recursive=False).strip()
+        moreDescription = body.find("span", class_="testhide")
+        for p in moreDescription.find_all("p", style="margin-top:-5px;"):
+            p.decompose()
+        moreDescription.find("span", class_="morelink list").decompose()
+        description = ogDescription + moreDescription.text
+
+        stats = body.find("div", class_="search_stats").find_all("span", class_="ss_desk")
+        chapters = stats[0].text.strip()
+        updateFreq = stats[1].text.strip()
+        nuReaders = stats[2].text.strip()
+        nuReviews = stats[3].text.strip()
+        lastUpdated = stats[4].text.strip()
+
+        genres = []
+        for genre in body.find("div", class_="search_genre").find_all("a"):
+            genreName = genre.text
+            genreLink = genre.get("href")
+            genres.append({
+                "name": genreName,
+                "link": genreLink
+            })
+        
+        results.append({
+            "title": title,
+            "link": link,
+            "image": image,
+            "search_rating": searchRating,
+            "description": description[:-1],
+            "chapters": chapters,
+            "update_freq": updateFreq,
+            "nu_readers": nuReaders,
+            "nu_reviews": nuReviews,
+            "last_updated": lastUpdated,
+            "genres": genres
+        })
+    return results
+
